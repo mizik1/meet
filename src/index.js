aee7d10d313c8d98a1ad84f3c4eed2779aba8544
mizik1 <mattk29@gmail.com>
2024-10-15 14:59:30 -0500

    Summary

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
+atatus.config("7f52a98d14744e99af2391dd15666c35").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
